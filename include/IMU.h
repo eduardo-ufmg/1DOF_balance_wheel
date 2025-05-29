c2c3ecf1189283edacbb958bcea4ca8e2c0ccfe6
eduardo-ufmg <eduardohbc@ufmg.br>
2025-05-29 17:19:28 -0300

    remove untracked

--- include/IMU.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-namespace IMU {
-    void init();
-    void update();
-    float getAngle();
-}
--- include/Motor.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-namespace Motor {
-    void init();
-    void update(float controlSignal);
-}
